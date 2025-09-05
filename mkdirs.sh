bebdf4e113fac4312e3fe418b8f98d209a3e3fc2
Khiem Doan <doankhiem.crazy@gmail.com>
2025-09-05 09:44:58 +0700

    Update Kafka 4.1
    Signed-off-by: Khiem Doan <doankhiem.crazy@gmail.com>

--- mkdirs.sh
+++ mkdirs.sh
@@ -9,23 +9,23 @@ mkdir -p .data/apicurio_db
 sudo chown -P 999:999 .data/apicurio_db
 
 # Kafka
-mkdir -p .data/kafka-1
-sudo chown 1000:1000 -R .data/kafka-1
+mkdir -p .data/controller-1
+sudo chown 1000:1000 -R .data/controller-1
 
-mkdir -p .data/kafka-2
-sudo chown 1000:1000 -R .data/kafka-2
+mkdir -p .data/controller-2
+sudo chown 1000:1000 -R .data/controller-2
 
-mkdir -p .data/kafka-3
-sudo chown 1000:1000 -R .data/kafka-3
+mkdir -p .data/controller-3
+sudo chown 1000:1000 -R .data/controller-3
 
-mkdir -p .data/kafka-4
-sudo chown 1000:1000 -R .data/kafka-4
+mkdir -p .data/broker-1
+sudo chown 1000:1000 -R .data/broker-1
 
-mkdir -p .data/kafka-5
-sudo chown 1000:1000 -R .data/kafka-5
+mkdir -p .data/broker-2
+sudo chown 1000:1000 -R .data/broker-2
 
-mkdir -p .data/kafka-6
-sudo chown 1000:1000 -R .data/kafka-6
+mkdir -p .data/broker-3
+sudo chown 1000:1000 -R .data/broker-3
 
-mkdir -p .data/kafka-7
-sudo chown 1000:1000 -R .data/kafka-7
+mkdir -p .data/broker-4
+sudo chown 1000:1000 -R .data/broker-4
